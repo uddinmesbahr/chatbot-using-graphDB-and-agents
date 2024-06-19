2b604ed530042a11949cddf80a24398ee2c5ef82
uddinmesbahr <107359488+uddinmesbahr@users.noreply.github.com>
2024-06-19 13:46:23 -0400

    Add files via upload

---
+++ chatbot_api/src/chains/__init__.py
@@ -0,0 +1,10 @@
+# # __all__ = [
+# #     "hospital_cypher_chain",      
+# #     "hospital_review_chain",  
+# # ]
+
+# __all__ = [
+#     "hospital_cypher_chain",      # refers to the 'echo.py' file
+#     "hospital_review_chain",  # refers to the 'surround.py' file
+#     # "reverse",   # !!! refers to the 'reverse' function now !!!
+# ]
